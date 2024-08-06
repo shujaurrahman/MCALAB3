127e34dd8efd91f8da74e1008372c23ae279c38b
shujaurrahman <Shujaurrehman210@gmail.com>
2024-08-07 02:37:22 +0530

    feat: w3p3

--- WEEK3/problem3.py
+++ WEEK3/problem3.py
@@ -1,1 +1,12 @@
 print("\nWrite a program to print characters from a string which are present at even index numbers.\n")
+
+def print_even_index_char(str):
+    '''print char at even index of string'''
+
+    for i in range(len(str)):
+        if i%2==0:
+            print(str[i],end=" ")
+    print()
+
+Str=input("Enter the string: ")
+print_even_index_char(Str)
